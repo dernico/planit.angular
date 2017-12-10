87e18089ff69f1443321b839aeaf1f55dc79c6a1
dern1co <nico.bolender.nb@gmail.com>
2017-12-10 12:36:15 +0100

    more ionic stuff

--- ionic/src/pages/app/app.component.ts
+++ ionic/src/pages/app/app.component.ts
@@ -3,8 +3,9 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { HomePage } from '../home/home';
+import { ListPage } from '../list/list';
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,7 +17,10 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -33,6 +37,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      console.log("initializeApp finished");
     });
   }
 
